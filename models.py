bf183b13d60c187875e5f864ac5dc1e98b51e0e0
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-19 17:13:39 +0200

    init alembic

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME
 
 DATABASE_URL = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
@@ -8,3 +8,4 @@ engine = create_engine(DATABASE_URL)
 
 session = sessionmaker(bind=engine)
 
+Base = declarative_base()
