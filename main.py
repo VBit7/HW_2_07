64f832ef3fa0d9f9d3a5d8f601c859b6d43e5d7e
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-21 17:07:13 +0200

    added argparse to main.py

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import argparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from connect_db import session
+from models import Groups, Students, Teachers, Subjects, Grades
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def create_model(args):
+    print(f"Creating {args.model} with name '{args.name}'")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def list_model(args):
+    print(f"Listing all {args.model}s")
+
+
+def update_model(args):
+    print(f"Updating {args.model} with id={args.id} to name '{args.name}'")
+
+
+def remove_model(args):
+    print(f"Removing {args.model} with id={args.id}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="CRUD operations with models")
+    parser.add_argument('--action', '-a', required=True, choices=['create', 'list', 'update', 'remove'],
+                        help='CRUD operation')
+    parser.add_argument('--model', '-m', required=True, help='Model name')
+    parser.add_argument('--id', type=int, help='ID of the model')
+    parser.add_argument('--name', help='Name of the model')
+
+    args = parser.parse_args()
+
+    action_functions = {
+        'create': create_model,
+        'list': list_model,
+        'update': update_model,
+        'remove': remove_model
+    }
+
+    action_functions[args.action](args)
+
+
+if __name__ == "__main__":
+    """
+    Приклад:
+
+    --action create -m Teacher --name 'Boris Jonson'            # створення вчителя
+    --action list -m Teacher                                    # показати всіх вчителів
+    --action update -m Teacher --id 3 --name 'Andry Bezos'      # оновити дані вчителя з id=3
+    --action remove -m Teacher --id 3                           # видалити вчителя з id=3
+    """
+
+    main()
